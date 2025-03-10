761cdcefe1bdefe39fefb83381400008ebdc4384
Olim <sayyid.olim12@gmail.com>
2025-03-10 14:20:23 +0700

    Add core module, add Firebase firestore for storage

---
+++ core/lib/common/name_router.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+const SPLASH_PAGE_ROUTE = '/splash';
+const LOGIN_PAGE_ROUTE = '/login';
+const NOTE_PAGE_ROUTE = '/note';
+const ADD_NOTE_PAGE_ROUTE = '/add-note';
+const EDIT_NOTE_PAGE_ROUTE = '/edit-note';
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,65 @@
+import 'package:auth/auth.dart';
+import 'package:core/common/name_router.dart';
+import 'package:catatanku/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:note/data/repositories/note_repositories_impl.dart';
+import 'package:note/domain/usecase/get_note_usecase.dart';
+import 'package:note/note.dart';
+import 'package:note/presentation/controller/get_note_cubit.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create:
+              (context) =>
+                  GetNoteCubit(GetNotesUseCase(NoteRepositoriesImpl())),
+        ),
+      ],
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  final _router = GoRouter(
+    initialLocation: '/note',
+    routes: <RouteBase>[
+      GoRoute(
+        name: LOGIN_PAGE_ROUTE,
+        path: '/login',
+        builder: (context, state) => LoginPage(),
+      ),
+      GoRoute(
+        name: NOTE_PAGE_ROUTE,
+        path: '/note',
+        builder: (context, state) {
+          return BlocProvider(
+            create:
+                (context) =>
+                    GetNoteCubit(GetNotesUseCase(NoteRepositoriesImpl())),
+            child: NotePage(),
+          );
+        },
+      ),
+      GoRoute(
+        name: ADD_NOTE_PAGE_ROUTE,
+        path: '/add-note',
+        builder: (context, state) => AddNotePage(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp.router(routerConfig: _router);
   }
 }
